51a5584ef0ff55976a3edeebb6db9e8bb2c38937
ivarog <42422542+ivarog@users.noreply.github.com>
2020-07-07 19:55:54 -0500

    Color menu added

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LevelManager : MonoBehaviour
+{
+    [Tooltip("Material que utilizará el carrito")]
+    [SerializeField] Material kartMaterial;
+    [Tooltip("Carrito a modificar")]
+    [SerializeField] GameObject kart;
+    
+    private SkinnedMeshRenderer kartMesh;
+
+    private void Update() {
+        kart.transform.Rotate(0f, 5f * Time.deltaTime, 0f);
+    }
+
+    public void ChangeColor(){
+        kartMaterial.color = EventSystem.current.currentSelectedGameObject.GetComponent<Image>().color;
+    }
+}
+
